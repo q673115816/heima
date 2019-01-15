58fa152a8ef8b623c5263b81c6d1d169115d6f8d
s871_linjinjun <673115816@qq.com>
2019-01-15 10:16:34 +0800

    feat(react_test): init react_test

---
+++ juejin_react_test/webpack-app/src/index.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+export default class Index extends PureComponent {
+    render() {
+        return (
+            <div>
+                Hello world React!
+                <span>123</span>
+                <p>444 5 </p>
+            </div>
+        )
+    }
+}
