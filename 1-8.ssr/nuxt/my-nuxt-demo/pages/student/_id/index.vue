8555131b36dab5c22c8ce200049bba6a6d6b895a
QQX <673115816@qq.com>
2019-03-05 23:18:28 +0800

    no message

---
+++ 1-8.ssr/nuxt/my-nuxt-demo/pages/student/_id/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>这是{{$route.params.id}}号学生的id</p>
+    </div>
+</template>
+
+<script>
+export default {
+    created() {
+        console.log(this.$route)
+    },
+    validate(obj) {
+        // console.log(obj)
+        return /^\d+$/.test(obj.params.id)
+    }
+}
+</script>
--- 1-8.ssr/nuxt/my-nuxt-demo/pages/teather.vue
+++ 1-8.ssr/nuxt/my-nuxt-demo/pages/teather.vue
@@ -1,18 +1,19 @@
 <template>
     <div>
-        <p>这是学生路由</p>
+        <p>这是老师路由</p>
         <ul>
-            <li v-for="item in student" :key="item.id">
-                <nuxt-link :to="`/student/${item.id}`">{{item.name}}</nuxt-link>
+            <li v-for="item in teather" :key="item.id">
+                <nuxt-link :to="`/teather/${item.id}`">{{item.name}}</nuxt-link>
             </li>
         </ul>
+    <nuxt-child/>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            student: [
+            teather: [
                 {name: 'qwe', id: 1},
                 {name: 'asd', id: 2},
                 {name: 'zxc', id: 3},
