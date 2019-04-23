1567691d6813f5cd3f94286b90de99470caa894d
s871_linjinjun <673115816@qq.com>
2019-04-23 18:22:05 +0800

    add geekvue

---
+++ geekvue/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/dashboard">dashboard</router-link> |
+      <router-link to="/forms">forms</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="less"></style>
---
+++ geekvue/src/layout/basicLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Header></Header>
+    <Sidemenu></Sidemenu>
+    <router-view></router-view>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "./header";
+import Footer from "./footer";
+import Sidemenu from "./sidemenu";
+export default {
+  components: {
+    Header,
+    Footer,
+    Sidemenu
+  }
+};
+</script>
+
+<style></style>
