0865b5d3ff0ceff04d23e6c4599d8b47a6d89b64
linjinjun <673115816@qq.com>
2019-07-10 11:43:10 +0800

    node

---
+++ node/深入浅出node/index.js
@@ -0,0 +1,8 @@
+var http = require('http')
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end('hello world\n')
+}).listen(8080, '127.0.0.1', function() {
+    
+})
